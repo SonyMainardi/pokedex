948a0cfbe2c92c96459dc06b685db42039612cbd
Sony Mainardi <141252102+SonyMainardi@users.noreply.github.com>
2023-12-21 16:38:42 -0300

    ultimas alterações

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,9 @@
 
-
-
 const pokeApi = {}
 
 function convertPokemonToLiToPokemon(pokeDetail){
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
